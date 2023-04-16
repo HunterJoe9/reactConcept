e5797889f87489d5132b68481b6bc5620ec03475
unknown <crossbone48@gmail.com>
2023-04-16 15:56:39 +0900

    up-down

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(1);
 
+  useEffect(() => {}, []);
   function plusNum() {
     setCount(count + 1);
   }
@@ -10,13 +11,13 @@ function App() {
   function substructNum() {
     setCount(count - 1);
   }
-  /* 작동 방식 : 
-    count의 state 초기값 = 0 
-    button 태그에 onClick 이벤트 부여 
+  /* 작동 방식 :
+    count의 state 초기값 = 0
+    button 태그에 onClick 이벤트 부여
     누르면 count 초기값 + 1 이 되는 함수명 작성
     함수 작성 : PlusNum
-    plusNum은 값을 렌더링해주는 SetCount 사용 
-    SetCount는 {count +1}을 인자로 받음 
+    plusNum은 값을 렌더링해주는 SetCount 사용
+    SetCount는 {count +1}을 인자로 받음
     렌더링 해준 값을 다시 초기값에 넣음
     반복 */
 
